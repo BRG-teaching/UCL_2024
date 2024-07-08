f363e16933f5e8e49c1362a651d75344a66be5bc
Alessandro Dell'Endice <34266891+adellendice@users.noreply.github.com>
2024-07-08 19:05:53 +0200

    Update 035_data_exchange_from_rhino.py

--- 035_data_exchange_from_rhino.py
+++ 035_data_exchange_from_rhino.py
@@ -1,10 +1,30 @@
 import compas
 import compas_rhino.objects
 import compas_rhino.conversions
+import pathlib
 
+
+# =============================================================================
+# mesh from rhino
+# =============================================================================
 guid = compas_rhino.objects.select_mesh()
-rmesh = compas_rhino.objects.find_object(guid)
-mesh = compas_rhino.conversions.mesh_to_compas(rmesh)
+mesh = compas_rhino.conversions.meshobject_to_compas(guid)
 
-filepath = pathlib.Path(__file__).parent / "data" / "mesh.json"
+# =============================================================================
+# lines from rhino
+# =============================================================================
+guids = compas_rhino.objects.select_lines()
+objects = [compas_rhino.objects.find_object(guid) for guid in guids]
+lines = [compas_rhino.conversions.curve_to_compas_line(obj.Geometry) for obj in objects]
+
+# =============================================================================
+# brep from rhino
+# =============================================================================
+guid = compas_rhino.objects.select_surface()
+surface = compas_rhino.conversions.brepobject_to_compas(guid)
+
+# =============================================================================
+# to json
+# =============================================================================
+filepath = pathlib.Path(__file__).parent / "data" / "mesh_rhino.json"
 compas.json_dump(mesh, filepath)
