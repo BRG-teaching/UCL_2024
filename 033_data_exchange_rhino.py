0728f23c69a2b0ba6debe79383c6b2b4be3720d9
Alessandro Dell'Endice <34266891+adellendice@users.noreply.github.com>
2024-07-08 11:27:34 +0200

    Update 033_data_exchange_rhino.py

--- 033_data_exchange_rhino.py
+++ 033_data_exchange_rhino.py
@@ -3,7 +3,7 @@ from compas.colors import Color
 from compas.geometry import Box
 from compas.scene import Scene
 
-filepath = "/Users/vanmelet/Code/BFH24/data/session.json"
+filepath = "/Users/adellend/c2/UCL_workshop/data/session.json"
 session = compas.json_load(filepath)
 
 box = session["box"]
