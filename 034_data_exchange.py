61cb3b9a4623069b82ece29460ead857f8d802be
Alessandro Dell'Endice <34266891+adellendice@users.noreply.github.com>
2024-07-09 08:37:41 +0000

    Update 034_data_exchange.py

--- 034_data_exchange.py
+++ 034_data_exchange.py
@@ -1,17 +1,17 @@
 import compas
+import pathlib
 from compas.colors import Color
 from compas.datastructures import Mesh
 from compas.geometry import Box
 from compas_viewer import Viewer
 
-filepath = "/Users/adellend/c2/UCL_workshop/data/session.json"
+filepath = pathlib.Path(__file__).parent / "data" / "session.json"
 session = compas.json_load(filepath)
 
 box: Box = session["box"]
 mesh: Mesh = session["mesh"]
 
 viewer = Viewer()
-viewer.scene.clear()
 viewer.scene.add(box, color=Color.red())
 viewer.scene.add(mesh, color=Color.green())
 viewer.show()
